949b17c84807acb852491c5a2f22d09989d55588
Денис Левицкий <levitskyandson@gmail.com>
2026-03-08 23:05:20 +0300

    chore: update build script and dependencies for T-one STT

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Устанавливаем системные пакеты для компиляции и работы
 apt-get update
-apt-get install -y ffmpeg
+apt-get install -y build-essential cmake ffmpeg
+
+# Устанавливаем Python-зависимости
 pip install -r requirements.txt
